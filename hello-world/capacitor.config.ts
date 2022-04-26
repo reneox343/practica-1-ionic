42a1ca589896c0aa529ac6ba6ac9fa0a0e5e3d9a
Rene <0228505@up.edu.mx>
2022-04-26 08:23:14 -0500

    new files

---
+++ hello-world/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'hello world',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
